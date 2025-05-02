f4733b2fe9f00a5146488f1a3845efbf68493965
Harsh Rakholiya <rakholiyaharsh332@gmail.com>
2025-05-02 10:24:34 +0530

    Middleware Or Set Route

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const  express = require('express');
+const { route } = require('./routes/products');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const products_routes = require("./routes/products");
+
 
 app.get("/", (req, res) => {
   res.send("Hello World! WelCome Harsh Website....");
 });
 
+// middleware or to set route
+app.use("/api/products", products_routes);
+
+
 const start = async () => {
     try {
         app.listen(PORT, () => {
-        console.log("${PORT} Yes Connect...");
+        console.log(`${PORT} Yes Connect...`);
         });
     }catch (error) {
         console.log(error);
